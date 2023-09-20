bf9437fbdb2e171fca0eecabbeba42fdf1476c72
Robin <robin.rh656@gmail.com>
2023-09-20 21:37:33 +0600

    update mongoose latest version

--- server.js
+++ server.js
@@ -9,17 +9,25 @@ const DB = process.env.DATABASE.replace(
   process.env.DATABASE_PASSWORD
 );
 
-(async function () {
-  await mongoose
-    .connect(DB, {
-      // .connect(process.env.DATABASE_LOCAL, {
-      useNewUrlParser: true,
-      useCreateIndex: true,
-      useFindAndModify: false,
-      useUnifiedTopology: true,
-    })
-    .then(() => console.log("DB connection successful!"));
-})();
+// (async function () {
+//   await mongoose
+//     .connect(DB, {
+//       // .connect(process.env.DATABASE_LOCAL, {
+//       useNewUrlParser: true,
+//       useCreateIndex: true,
+//       useFindAndModify: false,
+//       useUnifiedTopology: true,
+//     })
+//     .then(() => console.log("DB connection successful!"));
+// })();
+
+async function run() {
+  await mongoose.connect(
+    "mongodb+srv://robinrh656:PrFbaFod95Ql1eXT@cluster0.yroiteb.mongodb.net/natours?retryWrites=true&w=majority"
+  );
+}
+
+run();
 
 const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
