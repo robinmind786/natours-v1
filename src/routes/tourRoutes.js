cc7adb97cd562ad1eddecc119a309367eb43c09c
Robin <robin.rh656@gmail.com>
2023-09-20 21:16:36 +0600

    Testing for change into tourRoutes.js

--- src/routes/tourRoutes.js
+++ src/routes/tourRoutes.js
@@ -17,7 +17,7 @@ router.route("/top-5-cheap").get(aliasTopTours, getAllTours);
 router.route("/tour-stats").get(getTourStats);
 router.route("/monthly-plan/:year").get(getMonthlyPlan);
 
-router.route("/").get(protect, getAllTours).post(createTour);
+router.route("/").get(getAllTours).post(createTour);
 router.route("/:id").get(getTour).patch(updateTour).delete(deleteTour);
 
 module.exports = router;
