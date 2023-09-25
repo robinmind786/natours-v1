0776fb70e47b231dbbf47fc7131d2c67b8207fa4
Robin <robin.rh656@gmail.com>
2023-09-25 20:10:48 +0600

    TourModel add guides field

--- src/models/tourModel.js
+++ src/models/tourModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
+const User = require('./userModel');
 
 const tourSchema = new mongoose.Schema(
   {
@@ -98,6 +99,7 @@ const tourSchema = new mongoose.Schema(
         day: Number,
       },
     ],
+    guides: Array,
   },
   {
     toJSON: { virtuals: true },
@@ -105,6 +107,14 @@ const tourSchema = new mongoose.Schema(
     timestamps: true,
   },
 );
+
+// Add guides
+tourSchema.pre('save', async function (next) {
+  const guidesPromises = this.guides.map(async (id) => await User.findById(id));
+  this.guides = await Promise.all(guidesPromises);
+
+  next();
+});
 
 // create slug by the tour name
 tourSchema.pre('save', function (next) {
